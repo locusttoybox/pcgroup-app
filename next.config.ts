fa48f2d43075ff338f5d1bbd496d90ee1ec7c790
locusttoybox <locusttoybox@gmail.com>
2024-10-28 12:05:25 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+    cssModules: true,
+    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+    webpack: (config, { dev, isServer }) => {
+      // Customize the webpack config before returning
+      return config;
+    },
+    async redirects() {
+      return Promise.resolve([
+        {
+          source: '/old-page',
+          destination: '/new-page',
+          permanent: true,
+        },
+      ]);
+    
+},
+}
+
 
 export default nextConfig;
